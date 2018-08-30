af6bf669574c3610f90c729655d341b1bcb950f7
MaryAnne45 <mary.jereh@gmail.com>
2018-08-30 15:36:14 +0300

    added a file

--- config/database.php
+++ config/database.php
@@ -57,8 +57,8 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => "amazonaws.com",
-            'port' => "5432",
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
